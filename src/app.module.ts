6045717b6957df8eedf947eb511729390c298f0f
Daniil Krynko <daniil.krynko@gmail.com>
2023-06-02 19:55:29 +0300

    Connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubscribersModule } from './subscribers/subscribers.module';
+import { ConfigService, ConfigModule } from "@nestjs/config";
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [SubscribersModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    SubscribersModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
